79fec04d499a5a510d7b2e98c610b3cd8b446f75
Supawan <dmdmmm@Supawans-MacBook-Air.local>
2024-03-17 17:29:38 +0700

    17.29 17/03/2567

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest_starter'),
+    ProductsModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/orders/orders.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { Order, OrderDocument } from './schemas/order.schemas';
+import { Model } from 'mongoose';
+import { ProductsService } from '../products/products.service';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    @InjectModel(Order.name) private orderModel: Model<OrderDocument>,
+    private productsService: ProductsService,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto): Promise<Order> {
+    const productResult = await this.productsService.findOne(
+      createOrderDto.product,
+    );
+
+    if (!productResult) {
+      throw new NotFoundException('product not found');
+    }
+
+    const result = new this.orderModel(createOrderDto);
+    return result;
+  }
+
+  async findAll(): Promise<Order[]> {
+    return this.orderModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Order> {
+    return this.orderModel.findById(id).populate('product').exec();
+  }
+
+  async removeAll() {
+    await this.orderModel.deleteMany().exec();
+    return { message: 'Delete orders successful' };
+  }
+}
