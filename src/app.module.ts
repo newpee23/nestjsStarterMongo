ed0a8f87b5af0d2736585d0f1edc11610904e218
Supawan <dmdmmm@Supawans-MacBook-Air.local>
2024-03-19 12:54:26 +0700

    12.54 19/03/2567

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest_starter'),
     ProductsModule,
     OrdersModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
