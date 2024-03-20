683515acb86cd0a6bfaf668f47594868721fca8b
newpee23 <139107392+newpee23@users.noreply.github.com>
2024-03-20 09:47:48 +0700

    09.48 20/03/2567

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,11 +7,16 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from "multer";
+import  * as path from "path";
 
 @Controller('products')
 export class ProductsController {
@@ -46,4 +51,23 @@ export class ProductsController {
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
+  
+  @UseGuards(JwtAuthGuard)
+  @Post("/upload/:filename")
+  @UseInterceptors(FileInterceptor('file' , {
+    storage : diskStorage({
+      destination : "./uploads",
+      filename : (_req: any , file: { originalname: string, filename: string }, cb: (error: Error | null, filename: string) => void) => {
+       
+        const timestamp = Date.now();
+        const fileExtension = path.extname(file.originalname);
+        const uniqueFileName = `${timestamp}-${_req?.params?.filename}${fileExtension}`;
+        cb(null , uniqueFileName);
+      }
+    })
+  }))
+  async uploadFile(@UploadedFile() file : { fieldname: string, originalname: string, encoding: string, mimetype: string, size: number, destination: string, filename: string, path: string, buffer: Buffer }, @Param('filename') filename: string) {
+    return `Upload successfuly ${file.filename}`;
+  }
 }
+
